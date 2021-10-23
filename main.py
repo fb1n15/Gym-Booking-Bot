748abd674c459a44677af275bc1d49760e3fa01a
Chandler <fb1n15@users.noreply.github.com>
2021-10-23 17:31:32 +0100

    Now we have entered the Booking interface

--- main.py
+++ main.py
@@ -2,32 +2,105 @@ import time
 import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import argparse
+from selenium import webdriver
 
 # https://stackoverflow.com/questions/40555930/selenium-chromedriver-executable-needs-to-be-in-path
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 def login(username, pw, driver):
-    WebDriverWait(driver, 2).until(
+    WebDriverWait(driver, 5).until(
         EC.element_to_be_clickable((By.ID, 'loginUsername'))
         ).send_keys(username)
 
     driver.find_element_by_id('loginPassword').send_keys(pw)
 
-    driver.find_element_by_css_selector('#fm1 > div.form-group.text-center.submit-box > button').click()
+    driver.find_element_by_css_selector(
+        '#fm1 > div.form-group.text-center.submit-box > button').click()
     WebDriverWait(driver, 5)
 
 
 def book(gym_time, driver):
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH,
+                                    '//*[@id="_TargetedContent_WAR_luminis_INSTANCE_7auCc2KRa4O0_TCBlockPanel"]/div[3]/div/div/div/div/div/div/div/div[20]/div/div/p/a'))
+        ).click()
+
+    time.sleep(0.9)
+    # switch to the login tab
+    # https://www.browserstack.com/guide/how-to-switch-tabs-in-selenium-python
+    # prints parent window title
+    print("Parent window title: " + driver.title)
+
+    # get current window handle
+    p = driver.current_window_handle
+    print(f"Current window title: {p}")
+
+    # get first child window
+    chwd = driver.window_handles
+
+    for w in chwd:
+        # switch focus to child window
+        if w != p:
+            driver.switch_to.window(w)
+    print("Current window title: " + driver.title)
 
     try:
-        # Selects gym slot register button with corresponding time
-        driver.find_element_by_id('ctl00_ctl11_SearchTextBox').send_keys('Badminton 55 Mins')
-    except:
-        raise Exception('Cannot search Badminton 55 Mins slots')
+        WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '/html/body/div/nav/div/a'))
+            ).click()
+    except Exception:
+        print("fail to click the LOGIN button")
+        pass
+
+    print("Current window title: " + driver.title)
+    # input the username
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.NAME,
+                                    'loginfmt'))
+        ).send_keys('fb1n15@soton.ac.uk')
+
+    # click the next button
+    driver.find_element_by_id('idSIButton9').click()
+
+    # input the password
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.NAME,
+                                    'passwd'))
+        ).send_keys('Fanbi12345')
+
+    # click the next button
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.ID,
+                                    'idSIButton9'))
+        ).click()
+
+    # in case for the stay signed in page
+    try:
+        WebDriverWait(driver, 5).until(
+            EC.element_to_be_clickable((By.ID,
+                                        'idSIButton9'))
+            ).click()
+    except Exception:
+        pass
+
+    # click make a booking button
+    # input the password
+    WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH,
+                                    "/html/body/div/main/div/div/div[2]/div/div[2]/form/input[2]"))
+        ).click()
+
+    print("Current window title: " + driver.title)
+
+    WebDriverWait(driver, 60).until(
+        EC.element_to_be_clickable((By.XPATH,
+                                    '/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[3]/div/div[2]/input'))
+        ).send_keys('Fanbi12345')
 
 
 def main(gym_time):
@@ -44,7 +117,7 @@ def main(gym_time):
     driver.get("https://sussed.soton.ac.uk/")
 
     login('fb1n15', 'Fanbi12345', driver)
-    time.sleep(10)
+    book(17, driver)
 
 
 # Press the green button in the gutter to run the script.
